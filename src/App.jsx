9f06132fb3b7b0a9deaad3bc7c24f8a369f63bcc
Diego Padilla <dpadlara@ciencias.unam.mx>
2025-07-16 07:55:58 -0600

    creates dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="text-2xl text-slate-700">Hi</p>
-    </>
-  )
+  return <p className="text-2xl text-slate-700">Swith to dev branch</p>;
 }
 
 export default App;
