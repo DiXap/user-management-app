b18c541a1f648544d6ad434b1e62ca08c0e31b6a
Diego Padilla <dpadlara@ciencias.unam.mx>
2025-07-17 10:31:08 -0600

    feat: adds new button to add Users to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList";
+import UserAddButton from "./components/UserAddButton";
 import { UserProvider } from "./contexts/UserContext";
 
 function App() {
   return (
     <UserProvider>
+      <UserAddButton className="w-full" />
       <UserForm />
       <UserList />
     </UserProvider>
