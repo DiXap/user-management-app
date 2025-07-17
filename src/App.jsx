a3b5bdeb1ecbf99fb1865aef2e885dc334826edc
Diego Padilla <dpadlara@ciencias.unam.mx>
2025-07-17 16:54:36 -0600

    Feat: navbar (#5)
    * feat: introduces a Navbar component
    
    * feat: adds Navbar to main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import UserForm from "./components/UserForm";
 import UserList from "./components/UserList";
 import { UserProvider } from "./contexts/UserContext";
 import Toolbar from "./components/Toolbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <UserProvider>
+      <Navbar />
       <Toolbar />
       <UserForm />
       <UserList />
